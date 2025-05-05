c0b357c8f720f552f43a5cb210380dc9ffa75777
Stéphane Denommé <stephane.denomme@outlook.com>
2025-05-05 16:02:00 -0400

    Refactor CI/CD (#4)
    * Added needs on push
    
    * Added filters to jobs
    
    * Name jobs
    
    * Added gitignore and tests
    
    * Fix reporter
    
    * Fixed permissions
    
    * Added filter to upload artifact
    
    * Added filter
    
    * Added test types

---
+++ tests/ctf-sandbox.tests/ComponentTests/PlaceHolderTests.cs
@@ -0,0 +1,12 @@
+﻿namespace ctf_sandbox.tests.ComponentTests;
+
+public class PlaceHolderTests
+{
+    [Trait("Category", "Component")]
+    [Fact]
+    public void Test1()
+    {
+        Assert.True(true);
+    }
+}
+
