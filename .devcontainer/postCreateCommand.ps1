29f56609bdf7d4510e5e73270c26b37419d31325
CurlyFire <stephane.denomme@outlook.com>
2025-06-19 10:47:40 +0000

    Moved playwright install to powershell

--- .devcontainer/postCreateCommand.ps1
+++ .devcontainer/postCreateCommand.ps1
@@ -10,4 +10,7 @@ git config --global --add safe.directory $workspaceRoot
 Write-Host "🔧 Installing dotnet-ef tool..."
 dotnet tool install --global dotnet-ef
 
+Write-Host "Installing Playwright browsers..."
+Set-Location tests/ctf-sandbox.tests && dotnet build && ./bin/Debug/net9.0/playwright.ps1 install-deps && ./bin/Debug/net9.0/playwright.ps1 install
+
 Write-Host "✅ postCreateCommand script complete."
