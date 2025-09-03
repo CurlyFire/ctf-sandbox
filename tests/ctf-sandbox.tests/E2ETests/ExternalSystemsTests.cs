cfe562bab2b816461e497d2f16f7024887d29bb3
CurlyFire <stephane.denomme@outlook.com>
2025-09-03 11:55:18 +0000

    wip

---
+++ tests/ctf-sandbox.tests/E2ETests/ExternalSystemsTests.cs
@@ -0,0 +1,23 @@
+using Microsoft.Playwright;
+
+namespace ctf_sandbox.tests.E2ETests;
+
+public class ExternalSystemsTests : WebServerPageTest
+{
+    public ExternalSystemsTests(WebServer webServer) : base(webServer)
+    {
+    }
+
+    [Trait("Category", "E2E")]
+    [Fact]
+    public async Task ShouldBeAbleToViewEmails()
+    {
+        await Page.GotoAsync(string.Empty);
+        await Page.GetByRole(AriaRole.Link, new() { Name = "Open Email" }).ClickAsync();
+        var mailpit = await Page.WaitForPopupAsync();
+        await mailpit.WaitForLoadStateAsync(LoadState.DOMContentLoaded);
+
+        await Expect(mailpit.GetByRole(AriaRole.Button, new() { Name = "Inbox" })).ToBeVisibleAsync();
+    }
+}
+
