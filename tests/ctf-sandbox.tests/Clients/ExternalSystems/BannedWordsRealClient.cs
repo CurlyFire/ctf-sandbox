c687e23143b2b909cbe822f997da7a33027c4a78
Stéphane Denommé <stephane.denomme@outlook.com>
2026-08-04 20:24:00 -0400

    Added clients to drivers (#84)

--- tests/ctf-sandbox.tests/Clients/ExternalSystems/BannedWordsRealClient.cs
+++ tests/ctf-sandbox.tests/Clients/ExternalSystems/BannedWordsRealClient.cs
@@ -1,8 +1,16 @@
+using System.Net.Http.Json;
+
 namespace ctf_sandbox.tests.Clients.ExternalSystems;
 
 public class BannedWordsRealClient : HealthyHttpClient
 {
     public BannedWordsRealClient(HttpClient client) : base(client)
     {
+    }
+
+    public async Task CreateBannedWordAsync(string word)
+    {
+        var response = await HttpClient.PostAsJsonAsync("/BannedWords", new { Word = word });
+        response.EnsureSuccessStatusCode();
     }
 }
--- tests/ctf-sandbox.tests/Drivers/ExternalSystems/APIBannedWordsDriver.cs
+++ tests/ctf-sandbox.tests/Drivers/ExternalSystems/APIBannedWordsDriver.cs
@@ -1,20 +1,18 @@
 
-using System.Net.Http.Json;
-using System.Text.Json;
+using ctf_sandbox.tests.Clients.ExternalSystems;
 
 namespace ctf_sandbox.tests.Drivers.ExternalSystems;
 
 public class APIBannedWordsDriver : IBannedWordsDriver
 {
-    private readonly HttpClient _httpClient;
+    private readonly BannedWordsRealClient _client;
 
-    public APIBannedWordsDriver(HttpClient httpClient)
+    public APIBannedWordsDriver(BannedWordsRealClient client)
     {
-        _httpClient = httpClient;
+        _client = client;
     }
     public async Task CreateBannedWordAsync(string word)
     {
-        var response = await _httpClient.PostAsJsonAsync("/BannedWords", new { Word = word });
-        response.EnsureSuccessStatusCode();
+        await _client.CreateBannedWordAsync(word);
     }
 }
